775f3f04de9d4fd1863d954754b71eb1513f670d
emarkees <rphillipse@gmail.com>
2023-05-20 07:06:32 -0600

    Add Home.jsx files

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookLogic from '../components/BookLogic';
+
+const Home = () => (
+  <>
+    <div className="wrapper">
+      <BookLogic />
+    </div>
+  </>
+);
+
+export default Home;
