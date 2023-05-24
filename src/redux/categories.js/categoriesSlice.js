58e9e35e36e9608f1ddd9a3433c6a06180a5f6ad
emarkees <rphillipse@gmail.com>
2023-05-24 15:41:10 -0600

    Add categoriesSlice files

---
+++ src/redux/categories.js/categoriesSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+  status: 'Under construction',
+};
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {},
+});
+
+export const { action: categoriesActions, reducer: categoriesReducer } = categoriesSlice;
