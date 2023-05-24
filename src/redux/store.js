4b528145ef96234b48f40213a1863d3003fb1a16
emarkees <rphillipse@gmail.com>
2023-05-24 17:00:43 -0600

    Update README.md files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
-import categoriesReducer from './categories.js/categoriesSlice';
+import { categoriesReducer } from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
